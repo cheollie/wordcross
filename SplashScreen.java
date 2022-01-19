cc72242a9f05d8d0793c0a39c4127c319ab59fe7
Chelsea W <80985676+cheollie@users.noreply.github.com>
2022-01-19 05:21:40 +0000

    [FINAL] splash screen code commented! and random code removed - chelc

--- SplashScreen.java
+++ SplashScreen.java
@@ -1,112 +1,98 @@
-// The "SplashScreen" class.
-import java.awt.*;
-import hsa.Console;
+/* 
+Name: Chelsea Wong & Annie Wong
+Teacher: Ms. Basaraba
+Date: January 18th, 2022
+Description: This program draws the SplashScreen screen for our Crossword game. 
+             This screen is displayed before the main menu and upon the launch
+             of the game. This screen draws the name of our game in a crossword
+             style which is our ISP.
+ISP: Crossword
+*/
 
-public class SplashScreen
+import java.awt.*; //gives access to java graphic related commands (ie. fonts and colors)
+import hsa.Console; //gives access to the Console class of the hsa library
+
+public class SplashScreen //draws the SplashScreen screen where it displays name of our game in a crossword style upon launch of the game
 {
-    static Console c;           // The output console
+    Console c; //declaration of instance variable of the Console class; the output console    
     
+    // declaration of font constants [variables are in full uppercase since they are constants are do not change throughout the program]        
+    final Font SPLASHSCREEN = new Font ("Montserrat", Font.BOLD, 42); // font for splash screen text for this screen
+    
+    // declaration of color constants [variables are in full uppercase since they are constants are do not change throughout the program]       
+    final Color LPURPLE = new Color(232,231,252);
+    final Color DPURPLE = new Color(87,88,208); 
+    final Color WHITE = new Color(255,255,255);     
+        
     public SplashScreen(Console con){
-	c = con;
+        c = con; //to use the Console object passed as a parameter when creating an instance of this class
     }
-    public void cross(int x, int y, int n){
-	for (int i = 0; i < n; i+=9){
-	    c.drawLine(x+i,y,x,y+i); 
-	    c.drawLine(x+i,y+n,x+n,y+i);
-	    c.drawLine(x+i+1,y,x,y+i+1); 
-	    c.drawLine(x+i+1,y+n,x+n,y+i+1);            
-	    //System.out.println((Math.min(i,n))+" "+y+" "+x+" "+(Math.min(i,n))); 
-	    //c.drawLine(x+i+1,y+n,x+n,y+i+1);   
-	}
-	//c.println(x+" "+y+" "+n);
+    public void cross(int x, int y, int n){ //method used to draw diagonals on the "blocked" boxes on the board
+        for (int i = 0; i < n; i+=9){ //uses a for loop to draw diagonal lines with a given increment between each line
+            c.drawLine(x+i,y,x,y+i); 
+            c.drawLine(x+i,y+n,x+n,y+i);
+            c.drawLine(x+i+1,y,x,y+i+1); 
+            c.drawLine(x+i+1,y+n,x+n,y+i+1); 
+        }
     }
     public void splashScreen(){
-	c.clear();
-	c.println(c.getHeight()+" "+c.getWidth());
-	final Color LPURPLE = new Color(232,231,252);
-	final Color DPURPLE = new Color(87,88,208); 
-	final Color WHITE = new Color(255,255,255);         
-	final Font SPLASHSCREEN = new Font ("Montserrat", Font.BOLD, 42);
-	
-	//bg
-	c.setColor(DPURPLE); 
-	c.fillRoundRect(50,50,632,572,20,20);
-	c.setColor(LPURPLE);        
-	c.fillRoundRect(54,54,624,564,16,16); 
-	
-	c.setColor(DPURPLE); //shadow
-	c.fillRoundRect(183,189,371,298,6,6);   
-	c.fillRoundRect(179,185,371,298,6,6); 
-	  
-	c.setColor(WHITE); //bg of the board
-	c.fillRoundRect(183,189,363,290,2,2);
-	
-	//drawing the grid on the board
-	c.setColor(DPURPLE);
-	c.setFont(SPLASHSCREEN);
-	int n = 75; //size of each grid/square
-	int x = 181, y = 187;        
-	boolean[][] filled = new boolean[][] { {true, false, true, true, true}, {true, false, true, true, true}, {false, false, false, false, false}, {true, false, true, true, true}};         
-	for (int i = 0; i < 4; i++){
-	    for (int j = 0; j < 5; j++){
-		if (filled[i][j]){ 
-		    cross(j*(n-1)+x,i*(n-1)+y,n-3); //drawing the diagonal patterns on the filled ones
-		} 
-		c.drawRect(j*(n-1)+x,i*(n-1)+y,n,n);  
-		c.drawRect(j*(n-1)+x+1,i*(n-1)+y+1,n-2,n-2);  //thickening the line               
-		//System.out.println((j*(n-1)+x)+" "+(i*(n-1)+y));
-	    }
-	}        
-	String alteredTitle = "WORDCOSS";        
-	int[][] coords = new int[][] {{268,244},{275,317},{275,390},{274,461}, {203,390}, {349,390}, {428,390}, {499,390}};
-	for (int i = 0; i < 8; i++){
-	    c.drawString(alteredTitle.charAt(i)+"", coords[i][0], coords[i][1]);
-	    try {
-		Thread.sleep(400);
-	    } catch (Exception e){}            
-	} 
-	
-	try {
-	    Thread.sleep(500);
-	} catch (Exception e){}          
-	
-	
-	/*
-	int n = 60; //size of grid
-	int x = 172, y = 132;
-	boolean[][] filled = new boolean[][] { {true, false, true, true, true}, {true, false, true, true, true}, {false, false, false, false, false}, {true, false, true, true, true}};        
-	for (int i = 0; i < 4; i++){
-	    for (int j = 0; j < 5; j++){
-		if (filled[i][j]){ 
-		    c.setColor(Color.GRAY);
-		    c.fillRect(j*(n-1)+x,i*(n-1)+y,n,n);
-		    c.setColor(Color.BLACK);
-		}
-		c.drawRect(j*(n-1)+x,i*(n-1)+y,n,n);                
-		//System.out.println((j*(n-1)+x)+" "+(i*(n-1)+y));
-	    }
-	}
-	c.setFont( new Font ("Monospaced", Font.PLAIN, 30) );
-	
-	String alteredTitle = "WordCoss";
-	int[][] coords = new int[][] {{246+5,147+20},{246+5,206+20},{246+5,265+20},{246+5,324+20}, {187+5,265+20}, {305+5,265+20}, {364+5,265+20}, {423+5,265+20}};
-	for (int i = 0; i < 8; i++){
-	    c.drawString(alteredTitle.charAt(i)+"", coords[i][0], coords[i][1]);
-	    try {
-		Thread.sleep(500);
-	    } catch (Exception e){}            
-	}
-	*/
-	c.setColor(new Color(255,255,255,25));
-	for (int i = 0; i < 42; i++){ //gradient
-	    c.fillRect(0,0,720,660);
-	    try {
-		Thread.sleep(20);
-	    } catch (Exception e){}            
-	}
-	c.clear();
-	c.setColor(Color.BLACK);
-	c.setFont(new Font ("Serif", Font.PLAIN, 12));
-	
+        c.clear();
+        
+        //drawing the background [used across all screens]
+        c.setColor(DPURPLE); 
+        c.fillRoundRect(50,50,632,572,20,20);
+        c.setColor(LPURPLE);        
+        c.fillRoundRect(54,54,624,564,16,16); 
+        
+        //draws the shadow and outline of the board
+        c.setColor(DPURPLE); 
+        c.fillRoundRect(183,189,371,298,6,6);   
+        c.fillRoundRect(179,185,371,298,6,6); 
+          
+        //draws the background of the board
+        c.setColor(WHITE);
+        c.fillRoundRect(183,189,363,290,2,2);
+        
+        //drawing the grid on the board
+        c.setColor(DPURPLE);
+        c.setFont(SPLASHSCREEN);
+        int n = 75; //size of each grid/square
+        int x = 181, y = 187;        
+        boolean[][] filled = new boolean[][] { {true, false, true, true, true}, {true, false, true, true, true}, {false, false, false, false, false}, {true, false, true, true, true}};         
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 5; j++){
+                if (filled[i][j]){ 
+                    cross(j*(n-1)+x,i*(n-1)+y,n-3); //drawing the diagonal patterns on the filled ones
+                } 
+                c.drawRect(j*(n-1)+x,i*(n-1)+y,n,n);  
+                c.drawRect(j*(n-1)+x+1,i*(n-1)+y+1,n-2,n-2);  //thickening the line               
+            }
+        }     
+        
+        //animating the letters being typed   
+        String alteredTitle = "WORDCOSS";        
+        int[][] coords = new int[][] {{268,244},{275,317},{275,390},{274,461}, {203,390}, {349,390}, {428,390}, {499,390}};
+        for (int i = 0; i < 8; i++){
+            c.drawString(alteredTitle.charAt(i)+"", coords[i][0], coords[i][1]); //write each letter at a time given the coordinates from coords and the letters from alteredTitle
+            try {
+                Thread.sleep(400); //0.4 second delay between each letter (so it looks like it's typing!)
+            } catch (Exception e){}            
+        } 
+        
+        //delay after the letters being typed
+        try {
+            Thread.sleep(500); //half a second delay after writing the title before fading 
+        } catch (Exception e){}          
+        
+        
+        //fading the background by adding layers of translucent white
+        c.setColor(new Color(255,255,255,25));
+        for (int i = 0; i < 42; i++){
+            c.fillRect(0,0,720,660); //draws the rectangle
+            try {
+                Thread.sleep(20); //20 ms delay between adding each layer of translucent white
+            } catch (Exception e){}            
+        }
+        c.clear();        
     }
 } // SplashScreen class
